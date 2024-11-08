42965a015e98cad947bf3d6a30f0e26288864209
Fox-tamer <alaaud1007@gmail.com>
2024-11-08 14:26:02 +0200

    forLoop081124

---
+++ forLoop.cpp
@@ -0,0 +1,15 @@
+//«·Õ·ﬁ… «· ﬂ—«—Ì…
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i = 1;
+    for(  ;i<20; i +=5)// or (  ;i<20 ; i=i+5) - Not ( ;i<20; i+5)
+    {
+
+     cout<<i <<"- ";
+     cout << "Hello world!" << endl;
+    }
+
+return 0;
+}
